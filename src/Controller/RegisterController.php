63f4f425f7dee65b05eaa965202c41217e54a963
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-01 18:13:10 +0700

    commit

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Form\ClientTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+final class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'client_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $client = new Client();
+
+        $form = $this->createForm(ClientTypeForm::class, $client);
+
+        $form -> handleRequest($request);
+
+        if ($form -> isSubmitted() && $form -> isValid()) {
+            $entityManager -> persist($client);
+            $entityManager -> flush();
+        }
+
+        return $this->render('register/register.html.twig', [
+            'controller_name' => 'RegisterController',
+            'form' => $form -> createView(),
+        ]);
+    }
+}
