c7a6f13a512893cd92bfd0573f9c19d741ff4372
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-01 22:53:27 +0700

    added ScoringService to RegisterController

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Form\ClientTypeForm;
+use App\Service\ScoringService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,8 @@ use Symfony\Component\HttpFoundation\Request;
 final class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'client_register')]
-    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, EntityManagerInterface $entityManager,
+                            ScoringService $scoringService): Response
     {
         $client = new Client();
 
@@ -21,7 +23,14 @@ final class RegisterController extends AbstractController
 
         $form -> handleRequest($request);
 
+
+        $scoringResult = null;
+
         if ($form -> isSubmitted() && $form -> isValid()) {
+            $scoringResult = $scoringService -> calculate($client);
+            //Начисляются очки клиенту во время регистрации
+            $client -> setScoring($scoringResult -> total);
+
             $entityManager -> persist($client);
             $entityManager -> flush();
         }
@@ -29,6 +38,7 @@ final class RegisterController extends AbstractController
         return $this->render('register/register.html.twig', [
             'controller_name' => 'RegisterController',
             'form' => $form -> createView(),
+            'scoringResult' => $scoringResult
         ]);
     }
 }
