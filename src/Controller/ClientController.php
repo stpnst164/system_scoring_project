082a1c833cff5ef1316cd19f4908cccfe368815f
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-01 17:46:02 +0700

    created ClientController for Clients list

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
-    public function index(): Response
+    #[Route('/client', name: 'app_client', methods: ['GET'])]
+    public function index(ClientRepository $clientRepository): Response
     {
         return $this->render('client/index.html.twig', [
             'controller_name' => 'ClientController',
+            'clients' => $clientRepository -> findAll()
         ]);
     }
 }
