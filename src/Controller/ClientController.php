f55e9595d253e420732daba843d7bdf565c023e1
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-01 22:34:32 +0700

    added ScroingService to ClientController for scorings

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ClientRepository;
+use App\Service\ScoringService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +11,21 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ClientController extends AbstractController
 {
     #[Route('/client', name: 'app_client', methods: ['GET'])]
-    public function index(ClientRepository $clientRepository): Response
+    public function index(ClientRepository $clientRepository, ScoringService $scoringService): Response
     {
+        //Список клиентов
+        $clients = $clientRepository -> findAll();
+
+        //Подсчет очков клиента
+        $scorings = [];
+        foreach ($clients as $client) {
+            $scorings[$client -> getId()] = $scoringService -> calculate($client) -> getTotal();
+        }
+
         return $this->render('client/index.html.twig', [
             'controller_name' => 'ClientController',
-            'clients' => $clientRepository -> findAll()
+            'clients' => $clients,
+            'scorings' => $scorings
         ]);
     }
 }
