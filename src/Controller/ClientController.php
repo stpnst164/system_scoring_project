a36ecd7f2ab2749b4902e2e48b9addcd83de0663
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-02 16:53:42 +0700

    added pagination

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,14 +4,20 @@ namespace App\Controller;
 
 use App\Repository\ClientRepository;
 use App\Service\ScoringService;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client', methods: ['GET'])]
-    public function index(ClientRepository $clientRepository, ScoringService $scoringService): Response
+    #[Route('/clients', name: 'app_client', methods: ['GET'])]
+    public function index(
+        Request $request,
+        ClientRepository $clientRepository,
+        ScoringService $scoringService,
+        PaginatorInterface $paginator): Response
     {
         //Список клиентов
         $clients = $clientRepository -> findAll();
@@ -22,10 +28,23 @@ final class ClientController extends AbstractController
             $scoring[$client -> getId()] = $scoringService -> calculate($client) -> getTotal();
         }
 
+        //Запрос - сортировка по id - от большего к меньшему по скорингу
+        $queryBuilder = $clientRepository -> createQueryBuilder('c')
+            -> orderBy('c.id', 'DESC')
+            -> getQuery();
+
+        //Пагинация
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('client/index.html.twig', [
             'controller_name' => 'ClientController',
             'clients' => $clients,
-            'scoring' => $scoring
+            'scoring' => $scoring,
+            'pagination' => $pagination
         ]);
     }
 }
