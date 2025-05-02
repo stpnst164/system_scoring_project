de0f74c4fc752b955a32820bcfd69957b93a3973
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-02 18:59:11 +0700

    added show and edit functions

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use App\Form\ClientTypeForm;
 use App\Repository\ClientRepository;
 use App\Service\ScoringService;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -45,6 +48,35 @@ final class ClientController extends AbstractController
             'clients' => $clients,
             'scoring' => $scoring,
             'pagination' => $pagination
+        ]);
+    }
+
+    //Карточка клиента
+    #[Route('/clients/{id}, name: app_client_show', methods: ['GET'])]
+    public function show(Client $client): Response {
+        return $this -> render('client/show.html.twig', [
+            'client' => $client
+        ]);
+    }
+
+    //Редактирование карточки клиента
+    #[Route('/clients/{id}/edit, name: app_client_edit', methods: ['GET', 'POST'])]
+    public function edit(Client $client,
+                        Request $request,
+                        EntityManagerInterface $entityManager): Response {
+
+        $form = $this->createForm(ClientTypeForm::class, $client);
+
+        $form -> handleRequest($request);
+
+        if ($form -> isSubmitted() && $form -> isValid()) {
+            $entityManager -> flush();
+            return $this -> redirectToRoute('app_client');
+        }
+
+        return $this -> render('client/edit.html.twig', [
+            'client' => $client,
+            'form' => $form -> createView()
         ]);
     }
 }
