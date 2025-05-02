3b4ea6b6140ce5c6c830009eb7f62044cf78a82f
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-03 04:40:23 +0700

    added rules of scoring to ClientFixtures

--- src/DataFixtures/ClientFixtures.php
+++ src/DataFixtures/ClientFixtures.php
@@ -3,36 +3,53 @@
 namespace App\DataFixtures;
 
 use App\Entity\Client;
+use App\Service\ScoringService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
 class ClientFixtures extends Fixture
 {
+    private ScoringService $scoringService;
+
+    public function __construct(ScoringService $scoringService)
+    {
+        $this->scoringService = $scoringService;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('ru_RU');
 
-        $educations = [
-            'Среднее',
-            'Среднее специальное',
-            'Высшее'
+        $educations = ['Среднее', 'Специальное', 'Высшее'];
+        $phonePrefixes = [
+            'МегаФон' => ['+7923', '+7929', '+7933', '+7999'],
+            'Билайн'  => ['+7903'],
+            'МТС'     => ['+7913', '+7983'],
+            'Иной'    => ['+7950']
         ];
-
-        $agreements = [
-            'true',
-            'false'
-        ];
-
+        $emailDomains = ['gmail.com', 'yandex.ru', 'mail.ru', 'example.com'];
 
         for ($i = 0; $i < 20; $i++) {
             $client = new Client();
-            $client -> setFirstName($faker -> firstName);
-            $client -> setLastName($faker -> lastName);
-            $client -> setPhoneNumber($faker -> unique() -> numerify('+7##########'));
-            $client -> setEmail($faker -> email);
-            $client -> setEducation($faker -> randomElement($educations));
-            $client -> setGiveAgreement($faker -> randomElement($agreements));
+
+            $client->setFirstName($faker->firstName);
+            $client->setLastName($faker->lastName);
+
+            $prefix = $faker->randomElement($phonePrefixes[$faker->randomElement(array_keys($phonePrefixes))]);
+            $phone = $prefix . $faker->numerify('#######');
+            $client->setPhoneNumber($phone);
+
+            $domain = $faker->randomElement($emailDomains);
+            $email = $faker->userName . '@' . $domain;
+            $client->setEmail($email);
+
+            $client->setEducation($faker->randomElement($educations));
+            $client->setGiveAgreement($faker->boolean());
+
+            // Подсчёт скоринга
+            $scoring = $this->scoringService->calculate($client)->getTotal();
+            $client->setScoring($scoring);
 
             $manager->persist($client);
         }
