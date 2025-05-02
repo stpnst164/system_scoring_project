d850ff95b7fbdc4ce389b1aadf49a9a708006485
stpnst164 <anastasiia.stp170@gmail.com>
2025-05-02 19:41:32 +0700

    update

--- src/Dto/ScoringResult.php
+++ src/Dto/ScoringResult.php
@@ -4,14 +4,19 @@ namespace App\Dto;
 
 class ScoringResult
 {
-
     public function __construct(
-        public int $total
+        readonly int $total,
+        readonly array $breakdown
     )
     {}
 
     public function getTotal() :int
     {
         return $this -> total;
+    }
+
+    public function getBreakdown(): array
+    {
+        return $this->breakdown;
     }
 }
